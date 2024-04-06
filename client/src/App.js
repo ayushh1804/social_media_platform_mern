17b21a794b95f8345a5abfa5f20f745288ca8b23
AYUSH <ayushrajbls@gmail.com>
2024-04-06 17:05:11 +0530

    state store and theme setup

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,30 @@ import Homepage from "./scenes/homePage"
 import LoginPage from "./scenes/loginPage"
 import Navbar from "./scenes/navbar"
 import ProfilePage from "./scenes/profilePage"
-
-
+import { useMemo } from "react";
+import { useSelector } from "react-redux"
+import { cssBaseline, ThemeProvider } from "@mui/material"
+import { createTheme } from "@mui/material/styles"
+import { themeSettings } from "./theme";
 
 
 function App() {
-
+  const mode = useSelector((state) => state.mode)
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
 
   return (
 
     <div className="App">
       <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<Homepage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <cssBaseline />
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<Homepage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
